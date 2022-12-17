25370cd49262e3c609d65c8452dd756e5c6c1726
Paulo Monezi <paulo.monezi@icloud.com>
2022-12-17 07:17:44 -0300

    added handleChange event

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState, setState, useEffect } from 'react'
 
 export const Converter = (props) => {
+  const [currencyA, setCurrenyA] = useState("")
+  const [currencyB, setCurrencyB] = useState(0)
+  const [value, setValue] = useState('')
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
   return (
     <div className="converter">
-<h2>{props.currencyA} to BRL</h2>
+<h2>{props.currencyA} to {props.currencyB}</h2>
 {console.log(props.currencyA)}
-<input type="text"></input>
+<input type="text" onChange={handleChange}></input>
 <input type="button" value="convert"></input>
 <h2>Converted value</h2>
-
+{console.log(currencyA)}
     </div>
   )
 }
