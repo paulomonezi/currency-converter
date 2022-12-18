081019389243c7ccfdd9f513e18cbacbd1ea196c
Paulo Monezi <paulo.monezi@icloud.com>
2022-12-18 07:23:06 -0300

    changed to class component

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,22 +1,31 @@
-import React, { useState, setState, useEffect } from 'react'
+import React, { Component, useState, setState, useEffect } from 'react'
 
-export const Converter = (props) => {
-  const [currencyA, setCurrenyA] = useState("")
-  const [currencyB, setCurrencyB] = useState(0)
-  const [value, setValue] = useState('')
+export default class Converter extends Component {
 
-  const handleChange = (event) => {
-    setValue(event.target.value)
+    constructor(props){
+      super(props)
+    }
   }
 
-  return (
-    <div className="converter">
-<h2>{props.currencyA} to {props.currencyB}</h2>
-{console.log(props.currencyA)}
-<input type="text" onChange={handleChange}></input>
-<input type="button" value="convert"></input>
-<h2>Converted value</h2>
-{console.log(currencyA)}
-    </div>
-  )
-}
+
+
+// export const Converter = (props) => {
+//   const [currencyA, setCurrenyA] = useState("")
+//   const [currencyB, setCurrencyB] = useState(0)
+//   const [value, setValue] = useState('')
+
+//   const handleChange = (event) => {
+//     setValue(event.target.value)
+//   }
+
+//   return (
+//     <div className="converter">
+// <h2>{props.currencyA} to {props.currencyB}</h2>
+// {console.log(props.currencyA)}
+// <input type="text" onChange={handleChange}></input>
+// <input type="button" value="convert"></input>
+// <h2>Converted value</h2>
+// {console.log(currencyA)}
+//     </div>
+//   )
+// }
